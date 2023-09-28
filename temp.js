a9c579d145963022be242d9f3e33e6a8ae3b44b7
mellonaut <rleeazw@outlook.com>
2023-09-28 07:34:28 -0400

    Started graboid

---
+++ temp.js
@@ -0,0 +1,8 @@
+{
+    id: 20,
+    title: "Graboid and Pipelines: Iterations on a Stealer",
+    date: "September 28 2023",
+    description: "<p><h1>Thievery Corp</h1><p></p><img class='post-article__image' src='https://blog.straylightsecurity.com/assets/graboid/Sorrowset-V3.png' alt='Description of the image'><p></p></p><\/p>",
+    image: "https://blog.straylightsecurity.com/assets/graboid/Sorrowset-V3.png",
+    clip_path: "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)"
+  },
